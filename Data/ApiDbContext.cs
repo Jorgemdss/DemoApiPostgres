b3bf6ab065fc649e59c9ee12f50c576f1ab0ae47
Jorge Santos <jmdsantos4@gmail.com>
2024-03-24 17:43:37 +0000

    removed comments

--- Data/ApiDbContext.cs
+++ Data/ApiDbContext.cs
@@ -18,8 +18,6 @@ public class ApiDbContext : DbContext
                            .WithMany(r => r.Users)
                            .UsingEntity<UserRole>(
                                 userRole => userRole.HasKey(e => new { e.RoleId, e.UserId }));
-                                // userRole => userRole.HasOne<Role>().WithMany().HasForeignKey(r => r.RoleId),
-                                // userRole => userRole.HasOne<User>().WithMany().HasForeignKey(u => u.UserId),
     }
 
 
